e5c7b29945c97e66d18cf70a8edae1c199f28e42
Iggy <igoririanto@rocketmail.com>
2019-02-22 07:55:36 -0600

    add comments to describe curl commands

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import 'dotenv/config';
 import express from 'express';
+import bodyParser from 'body-parser';
 import cors from 'cors';
+import uuidv4 from 'uuid/v4';
 
 let users = {
   1: {
@@ -28,7 +30,14 @@ let messages = {
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
+app.use((req, res, next) => {
+  req.me = users[1]
+  console.log('req.me:', req.me);
+  next();
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`app is listening at port ${process.env.PORT}`);
@@ -59,29 +68,63 @@ app.delete('/users/:userId', (req, res) => {
     `DELETE HTTP method on user ${req.params.userId}`
   )
 });
-
+/*
+ * curl http://localhost:3000/messages
+*/
 app.get('/messages', (req, res) => {
-  return res.send(
-    `GET HTTP method on messages`
-  );
+  return res.send(JSON.stringify(messages));
 })
+/*
+ * curl http://localhost:3000/messages/1
+*/
 app.get('/messages/:messageId', (req, res) => {
   return res.send(
     `GET HTTP method on message ${req.params.messageId}`
   )
-})
-app.post('messages', (req, res) => {
-  return res.send(
-    `POST HTTP method on messages`
-  )
+}) 
+/*
+ * curl -X POST -H "Content-Type:application/json" http://localhost:3000/messages -d '{"text": "Updated text"}'
+*/
+app.post('/messages', (req, res) => {
+  const id = uuidv4();
+  console.log('req body:', req.body);
+  const message = {
+    id,
+    text: req.body.text,
+    userId: req.me.id
+  };
+
+  messages[id] = message;
+
+  return res.send(message);
 });
+/*
+ * curl -X PUT -H "Content-Type:application/json" http://localhost:3000/messages/1 -d '{"id": 1, "text": "Content updated!", "userId": 1}'
+*/
 app.put('/messages/:messageId', (req, res) => {
-  return res.send(
-    `PUT HTTP method on message ${req.params.messageId}`
-  )
+  const messageId = req.params.messageId;
+  if(messages[messageId]) {
+    messages[messageId] = req.body
+  };
+
+  return res.send(messages)
 });
+/* 
+ * curl -X DELETE http://localhost:3000/messages/1
+*/
 app.delete('/messages/:messageId', (req, res) => {
-  return res.send(
-    `DELETE HTTP method on message ${req.params.messageId}`
-  )
+  const {
+    [req.params.messageId]: message,
+    ...otherMessages
+  } = messages;
+
+  messages = otherMessages;
+  return res.send(messages)
 });
+
+/*
+ * curl http://localhost:3000/session
+*/
+app.get('/session', (req, res) => {
+  res.send(users[req.me.id])
+})
