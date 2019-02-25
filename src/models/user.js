af7656cafc01319a363e237ca5290ba3c724c07b
Iggy <igoririanto@rocketmail.com>
2019-02-25 07:47:06 -0600

    added message.js and user.js models

--- src/models/user.js
+++ src/models/user.js
@@ -1,28 +1,30 @@
 const user = (sequelize, DataTypes) => {
-	const User = sequelize.define('user', {
-		username: {
-			type: DataTypes.STRING,
-			unique: true
-		}
-	});
+  const User = sequelize.define('user', {
+    username: {
+      type: DataTypes.STRING,
+      unique: true,
+    },
+  });
 
-	User.associate = models => {
-		User.hasMany(models.Message);
-	};
+  User.associate = models => {
+    User.hasMany(models.Message, {onDelete: 'CASCADE'});
+  };
 
-	User.findByLogin = async login => {
-		let user = await User.findOne({
-			where: {username: login},
-		});
-		if(!user){
-			user = await User.findOne({
-				where: {email: login},
-			});
-		};
-		return user;
-	};
-	
-	return User;
-}
+  User.findByLogin = async login => {
+    let user = await User.findOne({
+      where: { username: login },
+    });
+
+    if (!user) {
+      user = await User.findOne({
+        where: { email: login },
+      });
+    }
+
+    return user;
+  };
+
+  return User;
+};
 
 export default user;
